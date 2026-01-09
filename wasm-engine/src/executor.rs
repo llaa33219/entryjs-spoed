6a633fc796d2ef3f4312ca46939f8e98e2c3355b
llaa33219 <lukems903@gmail.com>
2026-01-09 10:42:51 +0900

    함수 안 이동 디버그

--- wasm-engine/src/executor.rs
+++ wasm-engine/src/executor.rs
@@ -1686,6 +1686,7 @@ impl Executor {
     fn accumulate_brush_and_fill_path(&self, entity: &mut Entity) {
         if entity.brush_down {
             entity.frame_brush_path.push((entity.x, entity.y));
+            web_sys::console::log_1(&format!("accumulate: ({}, {}), stack_depth: {}", entity.x, entity.y, self.call_stack.len()).into());
         }
         if entity.fill_down {
             entity.frame_fill_path.push((entity.x, entity.y));
