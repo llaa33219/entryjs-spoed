05491c8ed37fe0df4cece84d5fe5130cf7933350
llaa33219 <lukems903@gmail.com>
2026-01-10 23:04:07 +0900

    오브젝트 역순 렌더링

--- wasm-engine/src/lib.rs
+++ wasm-engine/src/lib.rs
@@ -643,7 +643,6 @@ impl WasmEngine {
         
         let render_entities: Vec<RenderEntity> = inner.entities
             .iter()
-            .rev()
             .filter(|e| e.visible || e.brush_down || e.fill_down)
             .map(|e| RenderEntity::from(e))
             .collect();
